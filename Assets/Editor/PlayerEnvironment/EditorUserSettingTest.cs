7966aa9f9ccd9aff9e4237287bf3e0870eb2720a
ai2playgame <detective9684@gmail.com>
2023-01-07 23:22:34 +0900

    UserSettings以下をgit管理外に

---
+++ Assets/Editor/PlayerEnvironment/EditorUserSettingTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor.PlayerEnvironment
+{
+    public class EditorUserSettingTest
+    {
+        private const string MENU_NAME = "Config/";
+        private const string VAL_NAME = "Example";
+        
+        [MenuItem(MENU_NAME + "Set")]
+        private static void SetEditorUserSettings()
+        {
+            EditorUserSettings.SetConfigValue(VAL_NAME, "value");
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem(MENU_NAME + "Get")]
+        private static void GetEditorUserSettings()
+        {
+            var value = EditorUserSettings.GetConfigValue(VAL_NAME);
+            Debug.Log(value);
+        }
+    }
+}
