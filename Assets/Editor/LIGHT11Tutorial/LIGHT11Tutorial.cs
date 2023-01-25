3feb768a75fec73cc858bae5a2933f81a6895dee
ai2playgame <detective9684@gmail.com>
2023-01-25 23:01:40 +0900

    wip

---
+++ Assets/Editor/LIGHT11Tutorial/LIGHT11Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Editor.LIGHT11Tutorial
+{
+    public class LIGHT11Tutorial : EditorWindow
+    {
+        [MenuItem("LIGHT11/Example")]
+        public static void ShowWindow()
+        {
+            GetWindow<LIGHT11Tutorial>("Example");
+        }
+
+        private void OnEnable()
+        {
+            var root = rootVisualElement;
+            var box = new Box();
+            box.Add(new Button()
+            {
+                text = "Example Button"
+            });
+            box.Add(new Label()
+            {
+                text = "Example Button"
+            });
+
+            root.Add(box);
+        }
+    }
+}
