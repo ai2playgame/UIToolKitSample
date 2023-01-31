0926ca7dba25d787eec6baa11b3ad9cd50f9d321
ai2playgame <detective9684@gmail.com>
2023-02-01 00:00:26 +0900

    更新ボタンつけた

--- Assets/Editor/CollectInspectorWindow/CollectInspectorWindow.cs
+++ Assets/Editor/CollectInspectorWindow/CollectInspectorWindow.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Runtime.Scripts;
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Editor.CollectInspectorWindow
 {
@@ -18,18 +19,57 @@ namespace Editor.CollectInspectorWindow
         {
             Repaint();
         }
-        
+
         private void CreateGUI()
         {
-            var targets = FindObjectsOfType<SimpleObject>();
-
             var root = rootVisualElement;
 
-            foreach (var target in targets)
+            var toolbar = new Toolbar();
+            root.Add(toolbar);
+
+            var updatedButton = new ToolbarButton() { text = "Update" };
+            toolbar.Add(updatedButton);
+
+            // ボタンを押したらCollectObjectsが呼ばれるように
+            updatedButton.clicked += CollectObjects;
+
+            // 生成時に一回更新
+            CollectObjects();
+        }
+
+        private ListView _listView;
+
+        private void CollectObjects()
+        {
+            var root = rootVisualElement;
+
+            if (_listView != null)
             {
-                InspectorElement inspector = new InspectorElement(target);
-                root.Add(inspector);
+                root.Remove(_listView);
+                _listView = null;
             }
+
+            _listView = new ListView();
+
+            var targets = FindObjectsOfType<SimpleObject>().ToList();
+            targets.Sort((lhs, rhs) => String.Compare(lhs.name, rhs.name, StringComparison.Ordinal));
+
+            _listView.itemsSource = targets;
+            _listView.makeItem = () =>
+            {
+                var box = new VisualElement();
+                box.Add(new Label());
+                box.Add(new InspectorElement());
+                return box;
+            };
+            _listView.bindItem = (VisualElement element, int index) =>
+            {
+                (element.ElementAt(0) as Label).text = targets[index].name;
+                (element.ElementAt(1) as InspectorElement).Bind(new SerializedObject(targets[index]));
+            };
+
+            _listView.fixedItemHeight = 100;
+            root.Add(_listView);
         }
     }
 }
